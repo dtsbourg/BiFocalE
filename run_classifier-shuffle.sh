351dfdafc02601103be0f3fbddafca27aa3a8c75
Dylan Bourgeois <dylan.bourgeois@epfl.ch>
2019-02-12 14:37:29 -0800

    update scripts

--- run_classifier-shuffle.sh
+++ run_classifier-shuffle.sh
@@ -1,16 +1,16 @@
 MAGRET_DIR="sparse"
-PREFIX="sparse_varname_split_magret"
+PREFIX="sparse_fname2_split_magret"
 PRETRAIN_DIR="sparse"
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 
 python classifier.py \
   --do_train=True \
   --do_eval=False \
   --do_predict=True \
   --max_nb_preds=600 \
-  --task_name=varname \
-  --label_vocab=$MAGRET_DIR/sparse_tmp_vocab-code.txt \
+  --task_name=methodname \
+  --label_vocab=$MAGRET_DIR/sparse_fname2_vocab-label.txt \
   --vocab_file=$MAGRET_DIR/sparse_tmp_vocab-code.txt \
   --train_file=$MAGRET_DIR/${PREFIX}_tk.txt \
   --train_labels=$MAGRET_DIR/${PREFIX}_label.txt \
@@ -19,15 +19,15 @@ python classifier.py \
   --eval_labels=$MAGRET_DIR/${PREFIX}_label_val.txt \
   --eval_adj=$MAGRET_DIR \
   --data_dir=$MAGRET_DIR \
-  --output_dir=$MAGRET_DIR/cls_output-varname \
+  --output_dir=$MAGRET_DIR/cls_output-shuffle\
   --max_seq_length=64 \
   --train_batch_size=16 \
   --learning_rate=1e-6 \
-  --num_train_epochs=200 \
+  --num_train_epochs=500 \
   --save_checkpoints_steps=500 \
   --init_checkpoint=$PRETRAIN_DIR/pretraining_output-200k/model.ckpt-200000 \
   --bert_config_file=$PRETRAIN_DIR/bert_config.json \
   --sparse_adj=True \
   --adj_prefix=${PREFIX} \
   --clean_data=True \
-#  --shuffle=True
+  --shuffle=True
